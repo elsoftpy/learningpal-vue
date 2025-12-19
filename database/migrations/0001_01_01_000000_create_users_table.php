e1fde1233169dd4b73418164a018a0abc505d23d
elsoftpy <develop@elsoft.com.py>
2025-12-18 23:32:28 -0300

    fix: Ensure 'name' field in users table is unique

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('profile_id')->constrained('profiles');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
